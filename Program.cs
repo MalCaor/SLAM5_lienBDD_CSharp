e6731f0c60f1f514f38389e6580591b8a2129ccf
MalCaor <xav.lemen@orange.fr>
2019-09-06 11:01:59 +0200

    Lunch on FVille

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace SLAM5_lienBDD_CSharp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FVille());
         }
     }
 }
